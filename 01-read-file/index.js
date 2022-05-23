71261dbb5f7b787c1f43672ee110c17bb6cb91b7
Alexander Bobryshev <94990187+AlexBob98@users.noreply.github.com>
2022-05-24 01:17:28 +0300

    refactor: change to fs.ReadStream method

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const stream = fs.createReadStream(path.join(__dirname, 'text.txt'),'utf-8');
+const stream = fs.ReadStream(path.join(__dirname, 'text.txt'),'utf-8');
 
 stream.on('readable', function(){
     let data = stream.read();
