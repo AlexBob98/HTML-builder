563923e2da2eed579c2b526e4021315f7336c06e
Alexander Bobryshev <94990187+AlexBob98@users.noreply.github.com>
2022-05-23 15:17:20 +0300

    feat: finish task 01 read file

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'),'utf-8');
+
+stream.on('readable', function(){
+    let data = stream.read();
+    if (data != null) 
+    console.log(data);
+});
